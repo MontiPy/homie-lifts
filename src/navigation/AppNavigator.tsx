075ab2de76b662b04f242fa9d9399a214c58da91
MontiPy <michaeltontiveros@gmail.com>
2025-05-19 17:38:45 -0500

    fix app nav

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,16 +1,11 @@
 // src/navigation/AppNavigator.tsx
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeStack from "./HomeStack";
 import WorkoutStack from "./WorkoutStack";
-
-import HomeScreen    from '../screens/HomeScreen';
-import GroupsScreen  from '../screens/GroupsScreen';
-import WorkoutScreen from '../screens/WorkoutScreen';
-import WorkoutListScreen from '../screens/WorkoutListScreen';
 
 type TabParamList = {
   Home: undefined;
@@ -23,27 +18,30 @@ const Tab = createBottomTabNavigator<TabParamList>();
 
 export default function AppNavigator() {
   return (
-    <NavigationContainer >
-      <Tab.Navigator screenOptions={({ route }) => ({
-        headerShown: true,
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName: string;
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: true,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string = "ellipse-outline";
 
-          if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Workout') {
-            iconName = focused ? 'barbell' : 'barbell-outline';
-          }
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Workout") {
+              iconName = focused ? "barbell" : "barbell-outline";
+            }
 
-          return <Ionicons name={iconName as any} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: '#FF0000',
-        tabBarInactiveTintColor: 'gray',
-      })}
-    >
-        <Tab.Screen   name="Home"    component={HomeStack} />
+            return (
+              <Ionicons name={iconName as any} size={size} color={color} />
+            );
+          },
+          tabBarActiveTintColor: "#FF0000",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeStack} />
         {/* <Tab.Screen   name="Groups"  component={GroupsScreen} /> */}
-        <Tab.Screen   name="Workout" component={WorkoutStack} />
+        <Tab.Screen name="Workout" component={WorkoutStack} />
         {/* <Tab.Screen   name="Workouts" component={WorkoutListScreen} /> */}
       </Tab.Navigator>
     </NavigationContainer>
